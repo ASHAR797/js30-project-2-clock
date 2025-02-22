658def6253eaef66dd1b26e9565539bbcce715ff
ASHAR ALI <111843979+ASHAR797@users.noreply.github.com>
2025-02-22 20:15:58 +0500

    Updated files

--- app.js
+++ app.js
@@ -19,5 +19,20 @@ let clock = document.getElementById('digital-clock-value');
 
 setInterval(()=>{
    time = DigitalClock();
-    clock.innerHTML = time;
+   clock.innerHTML = time;
 } , 1000);
+const AnalogClock = () => {
+   setInterval(()=>{
+   let date = new Date();
+   let hours = date.getHours();
+   let minutes = date.getMinutes();
+   let seconds = date.getSeconds();
+   let hDeg =(hours * 30) + (minutes * 0.5) + (seconds * (0.5 / 60));
+   let mDeg = (minutes*6)+(seconds*0.1);
+   let sDeg = seconds * 6;
+   document.querySelector('.minute-hand').style.transform = `rotate(${mDeg}deg)`;
+   document.querySelector('.hour-hand').style.transform = `rotate(${hDeg}deg)`;
+   document.querySelector('.second-hand').style.transform = `rotate(${sDeg}deg)`;
+}, 1000);
+}
+AnalogClock();
